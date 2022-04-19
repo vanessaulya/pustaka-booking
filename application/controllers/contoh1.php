229b5bec42f86c346e844e2500921b033585d8d6
vanessaulya <vanessaulya20@gmail.com>
2022-04-19 17:36:11 +0700

    Tugas Pertemuan 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Prkenalkan<?/h1>";
+        echo "Nama saya Vanessa Ulya. Saya tinggal di Jarta Timur";
+    }
+}
