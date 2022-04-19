dde317b6e839cbc1ed7d8a26b00fd79ea483a42c
vanessaulya <vanessaulya20@gmail.com>
2022-04-19 15:33:03 +0700

    menambahkan Contohh1.php

---
+++ application/controllers/Contohh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contohh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalan</h1>";
+    echo "Nama saya Vanessa Ulya. Saya tinggal di Jakarta Timur";
+    }
+}
